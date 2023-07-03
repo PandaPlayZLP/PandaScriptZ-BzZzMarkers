cd15dc052d21a6b7bcaa6e4cbddb3e7ee87a85c8
PandaScriptZ <64916669+PandaPlayZLP@users.noreply.github.com>
2023-07-03 18:34:26 +0200

    Added predefined markers

--- config.lua
+++ config.lua
@@ -1,3 +1,23 @@
 Config = {}
 
 Config.AllowCommands = true -- Allow commands for spawning and despawning
+
+Config.Markers = {
+    {
+        coords = vector3(395.7025, -984.2512, 29.335),
+        type = "ambulance",
+        color = "blue",
+        light = true,
+        anim = true,
+        range = 10.0
+    },
+    {
+        coords = vector3(393.1204, -981.8807, 29.3959),
+        type = "drink",
+        color = "red",
+        light = true,
+        anim = true,
+        range = 5.0
+    },
+
+}
